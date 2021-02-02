afd64748e3184eac04661566790e9b9ecd032556
ZC-Huang <60633966+ZC-Huang@users.noreply.github.com>
2021-02-01 20:42:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wbdv_sp21-02-zichenghuang-server-java
+ WebDev A1
---
+++ src/test/java/com/example/wbdv_sp2102zichenghuangserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdv_sp2102zichenghuangserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
